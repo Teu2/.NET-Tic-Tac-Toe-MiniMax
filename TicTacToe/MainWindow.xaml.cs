8c7940ab5a4e39c499f7038c961f2c479a7b5e9d
Dominic Yeoh <yeohd100@gmail.com>
2022-12-28 18:22:06 +1100

    added partially working minimax

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -21,14 +21,24 @@ namespace TicTacToe
     public partial class MainWindow : Window
     {
         private MarkType[] results;
-        private string[,] board = new string[3, 3]; // minimax algo
+        private string[,] board = new string[3, 3]; // 2D array needed to apply MiniMax Algorithm
         private List<int> listIndex;
+        Dictionary<string, int> scores;
         private bool playerTurn;
         private bool gameEnd;
+        private bool MMGameEnd;
+        Pair bestMove = new();
 
         public MainWindow()
         {
             listIndex = new List<int>();
+            scores = new();
+            scores.Add("X", 1);
+            scores.Add("O", -1);
+            scores.Add("", 0);
+
+            //bestMove = new Pair();
+            //bestScore = int.MinValue;
             InitializeComponent();
             NewGame();
         }
@@ -42,7 +52,6 @@ namespace TicTacToe
                 results[i] = MarkType.Free;
             }
 
-            playerTurn = true; // true is player1
             playerTurn = true; // true is player1
 
             // iterates through every button on the grid
@@ -66,6 +75,9 @@ namespace TicTacToe
 
                 listIndex.Clear(); // clear index
 
+                bestMove = new Pair();
+                //bestScore = int.MinValue;
+                
                 // Initialize the game board to be empty
                 for (int row = 0; row < 3; row++)
                 {
@@ -77,10 +89,11 @@ namespace TicTacToe
 
             });
 
+            MMGameEnd = false;
             gameEnd = false;
         }
 
-        private void AiMove() // easy opponent
+        private void AiMove() // not really an AI
         {
             bool available = true;
 
@@ -110,6 +123,51 @@ namespace TicTacToe
             }
         }
 
+        private void AiBestMove() // partially working fix it
+        {
+            var bestScore = int.MinValue;
+            Pair pair = new(); // Best Move
+
+            CheckForWinner();
+            if (gameEnd == true) return;
+
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    if (board[r, c] == "")
+                    {
+                        board[r, c] = "X";
+                        var score = MiniMax(0, true);
+                        board[r, c] = "";
+
+                        if (score > bestScore)
+                        {
+                            bestScore = score;
+                            pair.Row = r;
+                            pair.Col = c;
+                        }
+                    }
+                }
+            }
+
+            var index = pair.Col + (pair.Row * 3);
+            
+
+            if (IsAvailable(index))
+            {
+                board[pair.Row, pair.Col] = "X";
+                results[index] = playerTurn ? MarkType.Cross : MarkType.Nought; // cell value based on player turn
+                listIndex.Add(index);
+
+                Button computerButton = (Button)Container.Children[pair.Row * 3 + pair.Col];
+                computerButton.Foreground = Brushes.Red;
+                computerButton.Content = "X";
+
+                playerTurn = true;
+            }
+        }
+
         private bool IsAvailable(int index)
         {
             if (!listIndex.Contains(index)) return true;
@@ -132,8 +190,7 @@ namespace TicTacToe
             var index = col + (row * 3);
 
             if (results[index] != MarkType.Free) return;
-
-
+            
             if (playerTurn)
             {
                 results[index] = playerTurn ? MarkType.Cross : MarkType.Nought; // cell value based on player turn
@@ -147,7 +204,8 @@ namespace TicTacToe
 
                 if (listIndex.Count != 9)
                 {
-                    AiMove();
+                    //AiMove();
+                    AiBestMove();
                 }
             }
             
@@ -253,6 +311,144 @@ namespace TicTacToe
                 rightColLine.Visibility = Visibility.Visible;
                 AlertWinner();
             }
+        }
+
+        // MiniMax functions =========================================================================================================
+
+        private int MiniMax(int depth, bool isMaximisingPlayer)
+        {
+            int score;
+            var result = MMCheckBoard();
+            
+
+            if (result != null)
+            {
+                return scores[result];
+            }
+
+            if (isMaximisingPlayer)
+            {
+                int bestScore = int.MinValue;
+                for (int r = 0; r < 3; r++)
+                {
+                    for (int c = 0; c < 3; c++)
+                    {
+                        if(MMIsAvailable(r, c))
+                        { 
+                            board[r, c] = "X";
+                            score = MiniMax(depth + 1, false);
+                            board[r, c] = "";
+                            bestScore = Math.Max(score, bestScore);
+                        }
+                    }
+                }
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = int.MaxValue;
+                for (int r = 0; r < 3; r++)
+                {
+                    for (int c = 0; c < 3; c++)
+                    {
+                        if (MMIsAvailable(r, c))
+                        {
+                            board[r, c] = "O";
+                            score = MiniMax(depth + 1, true);
+                            board[r, c] = "";
+                            bestScore = Math.Min(score, bestScore);
+                        }
+                    }
+                }
+                return bestScore;
+            }
+        }
+
+        private bool MMIsAvailable(int r, int c)
+        {
+            if (board[r, c] == "") return true;
+            return false;
+        }
+
+        private string MMCheckBoard()
+        {
+            string val = "";
+            // check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+                {
+                    val = board[row, 0]; return val;
+                }
+            }
+
+            // check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] == board[1, col] && board[1, col] == board[2, col])
+                {
+                    val = board[0, col]; return val;
+                }
+            }
+
+            // check diagonals
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                val = board[0, 0]; return val;
+            }
+
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                val = board[0, 2]; return val;
+            }
+
+            return val;
+        }
+
+        private int MMCheckBoard1()
+        {
+            // check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+                {
+                    if (board[row, 0] == "X")
+                        return +10;
+                    else if (board[row, 0] == "O")
+                        return -10;
+                }
+            }
+
+            // check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] == board[1, col] && board[1, col] == board[2, col])
+                {
+                    if (board[0, col] == "X")
+                        return +10;
+                    else if (board[0, col] == "O")
+                        return -10;
+                }
+            }
+
+            // check diagonals
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                if (board[0, 0] == "X")
+                    return +10;
+                else if (board[0, 0] == "O")
+                    return -10;
+            }
+
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                if (board[0, 2] == "X")
+                    return +10;
+                else if (board[0, 2] == "O")
+                    return -10;
+            }
+
+            return 0;
         }
     }
 }
---
+++ TicTacToe/Pair.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Pair
+    {
+        private int row;
+        private int col;
+        private int score;
+
+        public Pair()
+        {
+            row = Row;
+            col = Col;
+            score = MMScore;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public int MMScore { get; set; }
+    }
+}
